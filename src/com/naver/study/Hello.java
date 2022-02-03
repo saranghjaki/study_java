289f4e7e99d88a23fa14c038fbf9fa4d4aaafd47
youngsoo heo <3340513@naver.com>
2022-02-03 16:48:03 +0900

    k

---
+++ src/com/naver/study/Hello.java
@@ -0,0 +1,24 @@
+package com.naver.study;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java");
+		
+		int a;  //선언
+		a = 1;  //대입(선언만 하면 안되고 대입이되어야 냄비를 사용할수있다)
+		
+		//varchar string-> str;
+		
+		int b= 2;
+		
+		int c =0;
+	
+		c= a + b;
+		
+		System.out.println(c);
+	
+	}
+
+}
