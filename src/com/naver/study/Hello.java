38a2562af10c313ef3adbb100d5ab6e6dc2da2ea
youngsoo heo <3340513@naver.com>
2022-02-04 15:56:24 +0900

    s

--- src/com/naver/study/Hello.java
+++ src/com/naver/study/Hello.java
@@ -6,6 +6,8 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello java");
 		
+		
+		
 		int a;  //선언
 		a = 1;  //대입(선언만 하면 안되고 대입이되어야 냄비를 사용할수있다)
 		
@@ -26,4 +28,30 @@ public class Hello {
 
 //자바는 컴퓨터 언어이다  친구들은 c c++ 파이선
 //자바스크립트는 자바가 아니다
+//소프드웨어 jdk  만드는거 도와주는역할 컴파일러와 클래스 라이브러리를 포함하는 자바플랫폼 사양서의 구현(제일큰개념)
+//jre 만들어진걸 실행시켜주는게 목적 자바코드를 실행하기 위한 도구들로 구성된 패키지(그다음)
+//jvm 자바바이트코드를 os에 맞게 해석하여 실행해주는 역할 온니 실행 jre안에 개념(그그다음)
+
+
+//JAVA SE :대부분의 사람들이 가장 많이 접하는 표준 에디션 JAVA의 핵심 API와 기능제공
+//JAVA EE 기업에서 운영하는 서버 페이지에 특화된 에디션
+//JAVA ME :PDA나 셋톱박스 센서등의 임베디드(하드웨어장치에 시스템이 들어간) 시스템 환경에 특화된 에디션
+//JKVA FX 데시크톰 애플리케이션 개발 및 배포를 위한 에디션으로 크로스플랫폼 이식과 GUI 라이브러리를 제공
+//ORACLE JDK 오라클에서 배포 일부유료
+//OPEN JDK J2SE와 유사
+
+//소스코드 *.JAVA (사람이 볼수 있는거 작업한거)-> CTRL+S 하면 컴파일 저장->기계어 *.CLASS 파일로 만든다 
+
+//FUNCTION or Class or Package or Library
+
+//Function 기능
+//clss 파일
+//Package 폴더
+
+//1. jdk 
+//2. made by super man (called open source library)
+//apache-오픈소스
+//3. made by you
+//개발자
+
 
---
+++ src/com/naver/study/Variable.java
@@ -0,0 +1,112 @@
+package com.naver.study;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//변수 명명법
+		// 첫글자 소문자 그리고 다음 문자열 부터는 대문자
+		//모든 변수명은 붙여서 사용
+		String fullName ="heo youngsu";
+		String name ="youngsu";
+//		f2 누르면 상세한 설명 나옴2
+		//대소문자 구분함
+		System.out.println(name);
+		
+		
+		int roomNumber = 1;
+		int roomnumber = 2;
+		
+		System.out.println ("roomNumber: " + roomnumber);
+		System.out.println ("roomNumber: " +roomNumber);
+		
+		
+		//예약어는 사용 할 수 없다
+		//int int =2;
+	
+		
+		//변수1.선언 과 2.사용(2.1대입 2.2읽기)
+		//변수선언
+			
+		//변수 초기화(선언후 처음으로 값 저장)
+		int a;// 선언 (선언만으로 사용할 수 없음)
+		a=1; //대입
+		int b =1; //선언과 초기화
+		
+		int c = 0;
+		
+		c = a+ b;
+		//변수 초기화 하여야 메모리에 생성되고 연산에 이용 될 수 있다
+//		---------------------------------------------
+		
+//		그래서 변수 선언시 초기화를 하는 습관을 들이자
+		
+//		정수형은 0
+//		실수형은 0.0
+//		문자형은 '' 또는 ""
+		
+		boolean defaultNy = false;  //true
+		byte aa =0;
+		short bb= 0;
+		int cc = 0;
+		long dd= 0;
+		
+		float ee = 0.0f;
+		double ff = 0.0;
+		
+		char gg = ' ';
+		
+		String hh = "";
+//		----------------------------------
+		
+		//변수사용(변수값 저장& 변수값 읽음)
+		
+		//리터럴=데이터
+//		ex) 정수형리터럴 = 정수형데이터
+		
+		// 이스케이프 문자
+		System.out.println(c);		
+		System.out.println("heo youngsoo");		
+		System.out.println("heoyoungsu : " + c +" Genius");		
+		
+		System.out.println("한칸들여쓰기");		
+		System.out.println("\t 한칸들여쓰기");
+		System.out.println("\t\t 한칸들여쓰기");
+		
+		System.out.println("한줄바꿈");		
+		System.out.println(" \n한줄바꿈");	
+		
+		System.out.println("한줄바꿈2");		
+		System.out.println("\r한줄바꿈2");		
+		
+		System.out.println("");		
+		System.out.println("\'나는생각한다\'");		
+		
+		System.out.println("쏘크라테스가 말했다 \"나는 존재한다\"");		
+		System.out.println("\\");		
+		
+		System.out.print("asdfges");		
+		System.out.print("asdfges");		
+		System.out.print("\nasdfges");		
+		System.out.print("\r\nasdfges");		
+		System.out.println("\n\rasdfges");		
+		
+		
+		//boolean defaultNy = 1;
+		
+//		변수의범위
+		
+//		변수는 선언된 블럭 내에서만 사용 가능하다
+		
+		if(a == 1) {
+			int aaa  = 10;
+			int bbb = aaa +a;
+			System.out.println("bbb: "+ bbb);
+		}else {
+			
+		}
+		
+	}
+
+}
